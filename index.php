c4731915c1716f509d28b7a647808c3f1ce00a48
ligma <showmeurtits@protonmail.com>
2022-12-29 23:35:12 +0100

    style n stuff

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ $test = "hello";
 ?>
 <html>
 <head>
-
+<title>Start page</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
- <form action="/search.php">
-  <label for="search">What are you looking for ? :</label>
-  <input type="text" id="search" name="search"><br><br>
-  <input type="submit" value="Submit">
-</form> 
+  <form action="/search.php">
+    <label for="search">What are you looking for ? :</label>  <br>
+    <input type="text" id="search" name="search"><br><br>
+    <input type="submit" value="Submit">
+  </form> 
 
 </body>
 </html>
