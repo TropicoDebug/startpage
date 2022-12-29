4ec1011218ae7feda9ffc15338c2e30f93363356
TropicoDebug <simon.vanmello@std.heh.be>
2022-12-30 00:02:41 +0100

    center

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $test = "hello";
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <form action="/search.php">
-    <label for="search">What are you looking for ? :</label>  <br>
+    <form action="/search.php">
+    <label for="search">What are you looking for ? :</label><br>
     <input type="text" id="search" name="search"><br><br>
     <input type="submit" value="Submit">
   </form> 
