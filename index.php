b7a8eabf3d71936d9cfb2495043db3b5973844b9
TropicoDebug <simon.vanmello@std.heh.be>
2022-12-30 11:08:17 +0100

    google redirect

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
 
-$test = "hello";
+if (isset($_GET['submit'])){
+  $search = $_GET['search'];
+  header('Location: https://www.google.com/search?q='.$search);
+}
+
 ?>
 <html>
 <head>
@@ -8,11 +15,10 @@ $test = "hello";
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="/search.php">
+    <form action="index.php" method="GET">
     <label for="search">What are you looking for ? :</label><br>
     <input type="text" id="search" name="search"><br><br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" name="submit">
   </form> 
-
 </body>
 </html>
