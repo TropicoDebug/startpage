f3e6e3e69a923a5d6e922cc1de780dc7188e1007
TropicoDebug <simon.vanmello@std.heh.be>
2022-12-30 11:36:18 +0100

    + urlencode

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if (isset($_GET['submit'])){
-  $search = $_GET['search'];
+  $search = urlencode($_GET['search']);
   header('Location: https://www.google.com/search?q='.$search);
 }
 
