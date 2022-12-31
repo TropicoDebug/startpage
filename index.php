d5c38e01d5cb346d285c137b881c51de5d403fba
TropicoDebug <simon.vanmello@std.heh.be>
2022-12-31 22:40:06 +0100

    script.js

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if (isset($_POST['submit'])){
 <meta name = "viewport" content = "width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no">
 <title>Start page</title>
 <link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
   <div id="bg-image"></div>
@@ -21,31 +22,6 @@ if (isset($_POST['submit'])){
     <form action="index.php" method="POST">
     <label for="search" id="labelsearch" class="noselect">
       <script>
-        function sleep(ms) {
-          return new Promise(resolve => setTimeout(resolve, ms));
-        }
-        async function Tutor() {
-          var myText = "What are you looking for ";
-          var text = "";
-          for (let i = 0; i < myText.length ; i++) {
-            await sleep(Math.floor(Math.random() * 60) + 1);
-            text += myText[i];
-            document.getElementById('labelsearch').innerHTML = text;
-          }
-          document.getElementById('labelsearch').innerHTML = text + "<span id=\"blink\">?</span>";
-        }
-        function blinkChar() {
-          var elem = document.getElementById('blink');
-          if (elem.style.visibility === 'hidden') {
-            elem.style.visibility = 'visible';
-          } else {
-            elem.style.visibility = 'hidden';
-          }
-        }
-        async function callBlinkChar(){
-          await sleep(Math.floor(Math.random() * 500) + 1);
-          blinkChar();
-        }
         Tutor()
         setInterval(callBlinkChar, 700);
       </script>
---
+++ script.js
@@ -0,0 +1,28 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function Tutor() {
+    var myText = "What are you looking for ";
+    var text = "";
+    for (let i = 0; i < myText.length ; i++) {
+      await sleep(Math.floor(Math.random() * 60) + 1);
+      text += myText[i];
+      document.getElementById('labelsearch').innerHTML = text;
+    }
+    document.getElementById('labelsearch').innerHTML = text + "<span id=\"blink\">?</span>";
+}
+
+function blinkChar() {
+    var elem = document.getElementById('blink');
+    if (elem.style.visibility === 'hidden') {
+      elem.style.visibility = 'visible';
+    } else {
+      elem.style.visibility = 'hidden';
+    }
+}
+
+async function callBlinkChar(){
+    await sleep(Math.floor(Math.random() * 500) + 1);
+    blinkChar();
+}
