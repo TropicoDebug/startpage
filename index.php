168b670f560890163eb7fec69560f11765f998d4
TropicoDebug <simon.vanmello@std.heh.be>
2023-01-04 20:13:38 +0100

    test

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if (isset($_POST['submit'])){
       <input type="text" id="search" name="search" autofocus autocomplete="off" spellcheck="false">
       <input id="submit" type="submit" value="Submit" name="submit">
     </form> 
-    <footer id="bottom-container">
+    <div id="bottom-container">
       <div class="link-container">
         <a href="https://www.github.com" class="noselect">github</a>
       </div>
@@ -52,7 +52,7 @@ if (isset($_POST['submit'])){
       <div class="link-container">
         <a href="https://speedtest.net" class="noselect">speedtest</a>
       </div>
-    </footer>
+    </div>
   </main>
 <script>var exports = {};</script>
 <script type="text/javascript" src="script/script.js"></script>
